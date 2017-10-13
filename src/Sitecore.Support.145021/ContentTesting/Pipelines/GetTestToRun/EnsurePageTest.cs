9f786bca371fd89cc6ce2e7f07c6c8ce3421cc70
Sergey Plashenko <spl@sitecore.net>
2017-10-13 17:29:40 +0300

    Initial release

---
+++ src/Sitecore.Support.145021/ContentTesting/Pipelines/GetTestToRun/EnsurePageTest.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.ContentTesting.Model;
+using Sitecore.ContentTesting.Pipelines.GetTestToRun;
+
+namespace Sitecore.Support.ContentTesting.Pipelines.GetTestToRun
+{
+    public class EnsurePageTest : GetTestToRunProcessor
+    {
+        public override void Process(GetTestToRunArgs args)
+        {
+            if ((args.TestDefinition != null) && ((args.TestDefinition.GetTestType() == TestType.Page)))
+            {
+                var contentItem = args.TestDefinition.ParseContentItem();
+                if (contentItem.ItemID != args.HostItem.ID || contentItem.Language != args.HostItem.Language)
+                {
+                    args.TestDefinition = null;
+                }
+            }
+        }
+    }
+}
